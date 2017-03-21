526c8fbbc2c86e6b6ed2b72e680e1c1b7a179fdd
Khadim <Khadim@KhAdi__>
2017-03-21 11:36:35 -0700

    my Box drived

---
+++ src/example_class/Example_class.java
@@ -0,0 +1,16 @@
+         
+
+
+public class Example_class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Box b1=new Box(3,2,4,false);
+        
+System.out.println("the answer is"+b1);
+// TODO code application logic here
+    }
+    
+}
